340ce0f78e56186a4f1b385d954dc173efad25c1
dong <842668051@qq.com>
2017-12-28 08:26:07 +0800

    aaa

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -23,6 +23,7 @@ class HomeController extends Controller
 {
     public function index()
     {
+        echo 1;die;
         return Admin::content(function (Content $content) {
 
             $content->header('Dashboard');
