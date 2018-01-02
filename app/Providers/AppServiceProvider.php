0a0373e6bd5fa5282eeec71542b1484b2cb8a893
wqy <906555409@qq.com>
2018-01-02 20:32:25 +0800

    我

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use App\Category;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,6 +17,15 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+       // $list = Db::table('category')->get()->toArray();
+        
+       // var_dump($category);die;
+       // var_dump($list);die;
+
+        view()->composer('layout/index_head',function($view){
+            $category = $this->getOrderCategory();
+            $view->with('category',$category);
+        });
     }
 
     /**
@@ -25,5 +36,60 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+    }
+
+    public function getOrderCategory($arr=[],$pid=0){
+        $arr = [];
+        //  '男装/女装/内衣'=>[
+        //        '0' => [
+        //              'title'=>'男装'
+        //              'son'=>[
+        //                  'title' => '皮衣'
+        //              ]
+        //        ]
+        //  ]
+        // foreach($arr as $k => $v){
+        //         if($v->parent_id == $pid){
+        //             $tree[] = $v;
+        //             $tree = array_merge($tree,$this->getOrderCategory($arr,$v->id));
+        //         }
+        // }
+        // return $tree;
+        
+        // foreach($arr as $k => $v){
+        //     if($v->parent_id == $pid){
+        //         $v->son = $this->getOrderCategory($arr,$v->id);
+        //         $tree[] = $v;
+        //     }
+        // }
+            $res = Category::get(['id','title','parent_id','group_id'])->toArray();
+            foreach ($res  as $v){
+                if($v['parent_id']==0){
+                    $arr[$v['group_id']]['name']= isset($arr[$v['group_id']])?$arr[$v['group_id']]['name']."/".$v['title']:$v['title'];
+                    $arr[$v['group_id']]['id'][] = $v['id'];
+                }
+            }
+            foreach ($arr as $v){
+                $newArray[$v['name']] = $v['id'];
+            }
+            unset($arr);
+            $len = count($res);
+            foreach ($newArray as $k=>$v){
+                $array = [];
+                    for($i=0;$i<$len;$i++){
+                        if(in_array($res[$i]['parent_id'],$v)){
+                            $array[]=$res[$i];
+                        }
+                    }
+                    foreach ($array as $kk=>$vv){
+                        for($i=0;$i<$len;$i++){
+                            if($res[$i]['parent_id']==$vv['id']){
+                                $array[$kk]['child'][]=$res[$i];
+                            }
+                        }
+                    }
+                    $newArray[$k]=$array;
+            }
+        return $newArray;
     }
 }
