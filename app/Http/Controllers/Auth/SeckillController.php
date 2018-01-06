a43f09820ccbc44229ea1460a450206de6b9f6d2
liningjie <523809652@qq.com>
2018-01-06 11:25:31 +0800

    上传

--- app/Http/Controllers/Auth/SeckillController.php
+++ app/Http/Controllers/Auth/SeckillController.php
@@ -10,23 +10,27 @@ class SeckillController extends Controller
     //抢购
     public function seckill()
   {
-    $data = \App\Brand::orderBy('created_at','update_at')->get();
-		
+    $data = \App\Seckill::orderBy('created_at','update_at','seckill_price')->get()->toArray();
+    // dd($data);die;
+
+    foreach ($data as $key => $value) {
+      $goods=\App\Goods::where(['id'=>$data[$key]['goods_id']])->first()->toArray();
+      // dd($goods);
+      $data[$key]['goods']=$goods['name'];
+      $data[$key]['sell_price']=$goods['sell_price'];
+      $data[$key]['price']=$goods['promotion_price'];
+      $data[$key]['cover']=$goods['cover'];
+
+    }
+    // dd($data);
     return view('seckill/seckill',compact('data'));
   }
-
-  // public function show(){
-  // 	date_default_timezone_set('prc');//配置时差
-    
-  // 	 $time=time(request('time'));//数据库时间
-    
-  //    $now=strtotime(date("y-m-d h:i:s",time()));//当前时间戳
-     
-  // }
 
     //优惠券
     public function coupon()
     {
         return view('seckill/coupon');
     }
+
+
 }
