9f66fd0d05c4dcd08a85b28736c85748665f2b7a
lx <2273055345@qq.com>
2018-01-08 11:19:06 +0800

    lx

--- app/Http/Controllers/Auth/UserController.php
+++ app/Http/Controllers/Auth/UserController.php
@@ -62,9 +62,6 @@ class UserController extends Controller
 		}else{
 			$res = Address::create(['name'=>$data['name'],'amply'=>$data['amply'],'postcode'=>$data['postcode'],'phone'=>$data['phone'],'address'=>$data['address'],'user_id'=>$data['user_id'],'bulid'=>$data['bulid'],'is_default'=>$data['is_default']]);
 		}
-
-
-
 		if($res){
 			return redirect('user/address');
 		}
@@ -191,15 +188,4 @@ class UserController extends Controller
 		return view('user/safe');
 	}
 
-	//公用的header用户名 和登录状态
-//	public function status(){
-//		$status=IndexController::isLogin();
-//		if($status == false){
-//			return redirect('/login');
-//		}else{
-//			$user=Auth::user();
-//			$user->isLogin = $status;
-//			return $user;
-//		}
-//	}
 }
